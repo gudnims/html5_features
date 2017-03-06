342182de8bcd96ef5444bc9ce7e6dcb3c9a7271a
gudnims <gudnims@gmail.com>
2017-03-06 15:00:25 +0100

    end of day commit

--- js/myJs.js
+++ js/myJs.js
@@ -7,18 +7,18 @@ if (localStorage.visits) {
     localStorage.visits = JSON.stringify([]);
 }
 
-//$(function() {
-   // var visits = JSON.parse(localStorage.visits);
- //   if (visits.length > 15) {
-   //     visits.pop();
-   // }
-   // var time = new Date();
-   // var log = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "<br>";
-  //  visits.push(log);
-    //setModalContent(visits);
-  //  localStorage.visits = JSON.stringify(visits);
-    //document.getElementById("showTime").innerHTML = visits + "<br>";
-//});
+$(function() {
+    var visits = JSON.parse(localStorage.visits);
+    if (visits.length > 15) {
+        visits.pop();
+    }
+    var time = new Date();
+    var log = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "<br>";
+    visits.push(log);
+    setModalContent(visits);
+    localStorage.visits = JSON.stringify(visits);
+    document.getElementById("showTime").innerHTML = visits + "<br>";
+});
 
 function setModalContent(content) {
     var html = "<div class='text-center'>";
